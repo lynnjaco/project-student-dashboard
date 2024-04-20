6d673af6dce27d9ba62627455c361eceb019feb4
lynnjaco <jacobs.lynnette@gmail.com>
2024-04-20 16:36:27 -0400

    renders assesment completions for each student within the student componnent

--- src/Components/StudentCard.jsx
+++ src/Components/StudentCard.jsx
@@ -20,55 +20,55 @@ export const StudentCard = ( { student } ) => {
                     </div>
                     <p className="green-text underlined">Show More...</p>
                 </div>
-            </div>
-            <div>
                 <div>
-                    <h3>Codewars:</h3>
-                    <ul>
-                        <li>
-                            <p>Current Total: <span>{ student.codewars.current.total }</span></p>
-                        </li>
-                        <li>
-                            <p>Last Week: <span>{ student.codewars.current.lastWeek }</span></p>
-                        </li>
-                        <li>
-                            <p>Goal: <span>{ student.codewars.goal.total }</span></p>
-                        </li>
-                        <li>
-                            <p>Percent of Goal Achieved: <span>{ Math.round(student.codewars.current.total/student.codewars.goal.total*100) }%</span></p>
-                        </li>
-                    </ul>
-                </div>
-                <div>
-                    <h3>Scores</h3>
+                    <div>
+                        <h3>Codewars:</h3>
                         <ul>
                             <li>
-                                <p>Assignments: <span>{ student.cohort.scores.assignments * 100 }%</span></p>
+                                <p>Current Total: <span>{ student.codewars.current.total }</span></p>
                             </li>
                             <li>
-                                <p>Projects: <span>{ student.cohort.scores.projects * 100 }%</span></p>
+                                <p>Last Week: <span>{ student.codewars.current.lastWeek }</span></p>
                             </li>
                             <li>
-                                <p>Assessments: <span>{ student.cohort.scores.assessments * 100 }%</span></p>
+                                <p>Goal: <span>{ student.codewars.goal.total }</span></p>
+                            </li>
+                            <li>
+                                <p>Percent of Goal Achieved: <span>{ Math.round(student.codewars.current.total/student.codewars.goal.total*100) }%</span></p>
                             </li>
                         </ul>
-                </div>
-                <div>
-                    <h3>Certifications</h3>
-                        <ul>
-                            <li>
-                                <p>Resume: <span>{}</span></p>
-                            </li>
-                            <li>
-                                <p>LinkedIn: <span>{}</span></p>
-                            </li>
-                            <li>
-                                <p>Mock Interview: <span>{}</span></p>
-                            </li>
-                            <li>
-                                <p>GitHub: <span>{}</span></p>
-                            </li>
-                        </ul>
+                    </div>
+                    <div>
+                        <h3>Scores</h3>
+                            <ul>
+                                <li>
+                                    <p>Assignments: <span>{ student.cohort.scores.assignments * 100 }%</span></p>
+                                </li>
+                                <li>
+                                    <p>Projects: <span>{ student.cohort.scores.projects * 100 }%</span></p>
+                                </li>
+                                <li>
+                                    <p>Assessments: <span>{ student.cohort.scores.assessments * 100 }%</span></p>
+                                </li>
+                            </ul>
+                    </div>
+                    <div>
+                        <h3>Certifications</h3>
+                            <ul>
+                                <li>
+                                    <p>Resume: <span>{ student.certifications.resume ? "✅" : "❌" }</span></p>
+                                </li>
+                                <li>
+                                    <p>LinkedIn: <span>{ student.certifications.linkedin ? "✅" : "❌" }</span></p>
+                                </li>
+                                <li>
+                                    <p>Mock Interview: <span>{ student.certifications.github ? "✅" : "❌" }</span></p>
+                                </li>
+                                <li>
+                                    <p>GitHub: <span>{ student.certifications.mockInterview ? "✅" : "❌" }</span></p>
+                                </li>
+                            </ul>
+                    </div>
                 </div>
             </div>
         </li>
