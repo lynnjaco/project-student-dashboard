c9a18b320df5256fbc4554f29d7d3fa793409fc1
lynnjaco <jacobs.lynnette@gmail.com>
2024-04-24 20:08:10 -0400

    updates the h2 tag og the cohortstudentcount container to render the cohort group name of the student list currently on display, by referencing the cohortCode property of the frst student object of the student list array.  Alternate solution is needed because All Students will not display when clicked, because the allstudents string is not a vaalue for cohortCode

--- src/Components/CohortStudentCount.jsx
+++ src/Components/CohortStudentCount.jsx
@@ -3,7 +3,7 @@ import "./CohortStudentCount.css"
 export const CohortStudentCount = ( { students } ) => {
     return (
         <div className="cohort-student-count-container">
-            <h2>All Students</h2>
+            <h2>{ students[0].cohort.cohortCode }</h2>
             <p>Total Students: <span className="green-text">{ students.length }</span></p>
         </div>
     )
